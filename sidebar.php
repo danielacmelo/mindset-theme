3a20cd3e5eea84070334ff2b5249c3a64cd73525
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-05-06 12:43:02 -0700

    update tasks day 3

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,15 @@
  * @package FWD_Starter_Theme
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( ! is_active_sidebar( 'sidebar-1' )  && ! is_active_sidebar( 'sidebar-2' ) ) {
 	return;
 }
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <?php if ( is_page() && is_active_sidebar( 'sidebar-2' ) ) : ?>
+        <?php dynamic_sidebar( 'sidebar-2' ); ?>
+    <?php else : ?>
+        <?php dynamic_sidebar( 'sidebar-1' ); ?>    
+	<?php endif; ?>
 </aside><!-- #secondary -->
