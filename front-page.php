83e055fc4750c3031f898f1090f60633a63286c0
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-05-08 00:37:00 -0700

    task 2

--- front-page.php
+++ front-page.php
@@ -49,6 +49,7 @@ get_header();
                             <a href="<?php the_permalink(); ?>"> 
                                 <?php the_post_thumbnail( 'portrait-blog-crop' ); ?>
                                 <h3><?php the_title(); ?></h3>
+                                <?php echo get_the_date(); ?>
                             </a> 
                         </article>       
                         <?php 
