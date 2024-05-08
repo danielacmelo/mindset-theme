159543a92d9885f447f06d55590d39d955783404
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-05-08 10:00:06 -0700

    changed template tag footer

--- footer.php
+++ footer.php
@@ -24,8 +24,7 @@
             </nav>		
 		</div><!-- .footer-menus -->
 		<div class="site-info">
-            <a class="privacy-policy-link" href="<?php echo esc_url( get_permalink( '3' ) ); ?>"><?php esc_html_e( 'Privacy Policy' ); ?></a>
-            <br>
+            <?php the_privacy_policy_link(); ?><br>
 	        <?php esc_html_e( 'Created by ', 'fwd' ); ?><a href="<?php echo esc_url( __( 'https://wp.bcitwebdeveloper.ca/', 'fwd' ) ); ?>"><?php esc_html_e( 'Jonathon Leathers', 'fwd' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
