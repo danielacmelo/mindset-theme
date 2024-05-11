bda2cc53da5e3a922997d2dc54e5c3f555770fca
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-05-10 19:27:16 -0700

    update query front page to v3. h2 inside statement

--- front-page.php
+++ front-page.php
@@ -31,29 +31,33 @@ get_header();
             </section>
 
             <section class="home-work">
-                <h2>Featured Works</h2>
             <?php
             $args = array(
                 'post_type'      => 'fwd-work',
-                'posts_per_page' => 4,
+                'posts_per_page' => 4 
             );
+            
             $query = new WP_Query( $args );
-
-            if ( $query->have_posts() ) {
-                while( $query->have_posts() ) {
-                    $query->the_post(); 
-                    ?>
-                    <article>
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('medium'); ?>
-                            <h3><?php the_title(); ?></h3>
-                        </a>
-                    </article>
+            
+            if ( $query -> have_posts() ) : ?>
+                <section class="home-work">
+                    <h2>Featured Works</h2>
                     <?php
-                }
-                wp_reset_postdata();
-            } 
-            ?>
+                    while ( $query -> have_posts() ) :
+                        $query -> the_post(); 
+                        ?>
+                        <article>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail('medium'); ?>
+                                <h3><?php the_title(); ?></h3>
+                            </a>
+                        </article>
+                        <?php
+                    endwhile;
+                    wp_reset_postdata();
+                    ?>
+                </section>
+            <?php endif; ?>
             </section>
 
             <section class="home-work"></section>
