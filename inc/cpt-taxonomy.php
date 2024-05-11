07073f460720f815dc6e9814ef98f55bc70e3c51
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-05-11 12:03:27 -0700

    registered service cpt

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -137,6 +137,54 @@ function fwd_register_custom_post_types() {
     );
     register_post_type( 'fwd-job-posting', $args );
 
+    // Register Services Custom Post Type
+        $labels = array(
+            'name'               => _x( 'Services', 'post type general name' ),
+            'singular_name'      => _x( 'Service', 'post type singular name'),
+            'menu_name'          => _x( 'Services', 'admin menu' ),
+            'name_admin_bar'     => _x( 'Service', 'add new on admin bar' ),
+            'add_new'            => _x( 'Add New', 'service' ),
+            'add_new_item'       => __( 'Add New Service' ),
+            'new_item'           => __( 'New Service' ),
+            'edit_item'          => __( 'Edit Service' ),
+            'view_item'          => __( 'View Service' ),
+            'all_items'          => __( 'All Services' ),
+            'search_items'       => __( 'Search Services' ),
+            'parent_item_colon'  => __( 'Parent Services:' ),
+            'not_found'          => __( 'No services found.' ),
+            'not_found_in_trash' => __( 'No services found in Trash.' ),
+            'archives'           => __( 'Service Archives'),
+            'insert_into_item'   => __( 'Insert into service'),
+            'uploaded_to_this_item' => __( 'Uploaded to this service'),
+            'filter_item_list'   => __( 'Filter service list'),
+            'items_list_navigation' => __( 'Services list navigation'),
+            'items_list'         => __( 'Services list'),
+            'featured_image'     => __( 'Service featured image'),
+            'set_featured_image' => __( 'Set service featured image'),
+            'remove_featured_image' => __( 'Remove service featured image'),
+            'use_featured_image' => __( 'Use as featured image'),
+        );
+    
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'show_in_nav_menus'  => true,
+            'show_in_admin_bar'  => true,
+            'show_in_rest'       => true,
+            'query_var'          => true,
+            'rewrite'            => array( 'slug' => 'services' ),
+            'capability_type'    => 'post',
+            'has_archive'        => false,
+            'hierarchical'       => false,
+            'menu_position'      => 22,
+            'menu_icon'          => 'dashicons-archive',
+            'supports'           => array( 'title' ),
+        );
+        register_post_type( 'fwd-service', $args );
+
 }
 add_action( 'init', 'fwd_register_custom_post_types' );
 
