255f3f86ec95dfd02c8f6759d04f43bbfe1c3a17
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-05-22 22:50:59 -0700

    added rest api demo page

--- functions.php
+++ functions.php
@@ -217,6 +217,35 @@ function fwd_scripts() {
         );
     }
 
+    if ( is_page( 319 ) ) {
+
+        // Enqueue the CSS file.
+        wp_enqueue_style(
+            'fwd-rest-api-css',
+            get_template_directory_uri() . '/css/rest-api.css',
+            array(),
+            _S_VERSION
+        );
+    
+        // Enqueue the JS file.
+        wp_enqueue_script(
+            'fwd-rest-api',
+            get_template_directory_uri() . '/js/rest-api.js',
+            array(),
+            '20200528',
+            false
+        );
+    
+        // Create a JS object to store the URL for the JS file.
+        wp_localize_script(
+            'fwd-rest-api',
+            'rest_object',
+            array( 'api_url' => site_url( '/wp-json/wp/v2/' ) )
+        );
+    
+    }
+
+
 }
 add_action( 'wp_enqueue_scripts', 'fwd_scripts' );
 
--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -7,23 +7,65 @@
  * @package FWD_Starter_Theme
  */
 
-get_header(); ?>
+get_header();
+?>
 
 	<main id="primary" class="site-main">
 
-		<?php
+        <?php
 		while ( have_posts() ) :
 			the_post();
+        ?>   
 
-			get_template_part( 'template-parts/content', 'page' );
-			
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                </header><!-- .entry-header -->
 
-		endwhile; // End of the loop.
-		?>
+                <div class="entry-content">
+                    <?php
+                    the_content();
+
+            
+                    $args  = array(
+                        'post_type'      => 'post',
+                        'posts_per_page' => 3,
+                    );
+                    $query = new WP_Query( $args );
+                    ?>
+                    <?php if ( $query->have_posts() ) : ?>
+                        <section class="blog-posts">
+                            <h2>Latest Blog Posts</h2>
+                            <?php
+                            while ( $query->have_posts() ) :
+                                $query->the_post();
+                                ?>
+                                <a href="<?php the_permalink(); ?>" class="open-modal" data-id="<?php echo get_the_ID(); ?>">
+                                    <h3><?php the_title(); ?></h3>
+                                    <?php the_post_thumbnail( 'medium' ); ?>
+                                </a>
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata(); ?>
+                            <p class="all-posts"><a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">See All Posts</a></p>
+                        </section>
+                        <div id="modal-window" class="modal-window">
+                            <button id="close-modal" class="close-modal">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <title><?php _e( 'Close' ); ?></title>
+                                    <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"></path>
+                                </svg>
+                            </button>
+                            <div id="modal-window-content"></div>
+                        </div>
+                        <div id="dimmer"></div>
+                    <?php endif; ?>
+                            </div><!-- .entry-content -->
+             
+            </article><!-- #post-<?php the_ID(); ?> -->     
+            
+        <?php
+        endwhile;
+        ?>
 
 	</main><!-- #primary -->
 
